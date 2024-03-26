4eb006a27a52f71736776a5932cecf251a31274f
Charli_x1 <derrhalo50@gmail.com>
2024-03-26 20:23:29 +0000

    projects

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./app.scss"
 import { Hero } from "./components/Hero/Hero";
 import { NavBar } from "./components/NavBar/NavBar";
 import { Parallax } from "./components/Parallax/Parallax";
+import { Portfolio } from "./components/Portfolio/Portfolio";
 import { Services } from "./components/Services/Services";
 
 const App = () => {
@@ -13,9 +14,7 @@ const App = () => {
     <section id="Services"><Parallax type="services"/></section>
     <section><Services /></section>
     <section id="Portfolio"><Parallax type="portfolio"/></section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
+    <Portfolio />
     <section id="Contact">Contact</section>
   </div>;
 };
