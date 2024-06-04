24e57177e2f71073341d593ef5ee0c2038c84a69
yahya akli <akli.yahya05@gmail.com>
2024-06-04 16:21:01 +0100

    yahya

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -55,22 +55,22 @@ export const Services = () => {
                 <motion.div className="box" whileHover={{background:"gray", color: "black",transition: { duration: 0.3 },scale: 0.98,}}>
                     <h2>Branding</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, aspernatur ratione illo, autem eos tenetur culpa doloribus hic deleniti possimus ullam, magni optio quo voluptate amet neque dolore sequi blanditiis!</p>
-                    <motion.button whileHover={{transition: { duration: 0.3, ease: "easeOut" },scale: 0.96,}}>Go</motion.button>
+                <a href="#Contact"><motion.button whileHover={{transition: { duration: 0.3, ease: "easeOut" },scale: 0.96,}}>Go</motion.button></a>
                 </motion.div>
                 <motion.div className="box" whileHover={{background:"gray", color: "black",transition: { duration: 0.3 },scale: 0.98}}>
                     <h2>Branding</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, aspernatur ratione illo, autem eos tenetur culpa doloribus hic deleniti possimus ullam, magni optio quo voluptate amet neque dolore sequi blanditiis!</p>
-                    <motion.button whileHover={{transition: { duration: 0.3, ease: "easeOut" },scale: 0.96,}}>Go</motion.button>
+                <a href="#Contact"><motion.button whileHover={{transition: { duration: 0.3, ease: "easeOut" },scale: 0.96,}}>Go</motion.button></a>
                 </motion.div>
                 <motion.div className="box" whileHover={{background:"gray", color: "black",transition: { duration: 0.3 },scale: 0.98,}}>
                     <h2>Branding</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, aspernatur ratione illo, autem eos tenetur culpa doloribus hic deleniti possimus ullam, magni optio quo voluptate amet neque dolore sequi blanditiis!</p>
-                    <motion.button whileHover={{transition: { duration: 0.3, ease: "easeOut" },scale: 0.96,}}>Go</motion.button>
+                <a href="#Contact"><motion.button whileHover={{transition: { duration: 0.3, ease: "easeOut" },scale: 0.96,}}>Go</motion.button></a>
                 </motion.div>
                 <motion.div className="box" whileHover={{background:"gray", color: "black",transition: { duration: 0.3 },scale: 0.98,}}>
                     <h2>Branding</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, aspernatur ratione illo, autem eos tenetur culpa doloribus hic deleniti possimus ullam, magni optio quo voluptate amet neque dolore sequi blanditiis!</p>
-                    <motion.button whileHover={{transition: { duration: 0.3, ease: "easeOut" },scale: 0.96,}}>Go</motion.button>
+                <a href="#Contact"><motion.button whileHover={{transition: { duration: 0.3, ease: "easeOut" },scale: 0.96,}}>Go</motion.button></a>
                 </motion.div>
             </motion.div>
         </motion.div>
