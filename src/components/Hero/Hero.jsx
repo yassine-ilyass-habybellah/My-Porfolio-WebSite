30e46a0b0967a37d398e170c690f8aab91ccb6b9
yahya akli <akli.yahya05@gmail.com>
2024-06-04 16:01:51 +0100

    yahya

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -48,8 +48,8 @@ export const Hero = () => {
                     <motion.h2 variants={textVariants}>OUSSAMA DARRHAL</motion.h2>
                     <motion.h1 variants={textVariants}>Full Stack Web Developer.</motion.h1>
                     <motion.div variants={textVariants} className="buttons">
-                        <motion.button variants={textVariants}><a href="#Portfolio">See Latest Work</a></motion.button>
-                        <motion.button variants={textVariants}><a href="#Contact">Contact Me</a></motion.button>
+                    <a href="#Portfolio"><motion.button variants={textVariants}>See Latest Work</motion.button></a>
+                    <a href="#Contact"><motion.button variants={textVariants}>Contact Me</motion.button></a>
                     </motion.div>
                     <motion.img variants={textVariants} animate="scrollButton" src="./scroll.png" alt="" />
                 </motion.div>
